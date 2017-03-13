35c8cdd72ec6935116962c466d4233964ff2ca6c
Sujina <shalesh@qwinix.io>
2017-03-13 17:46:50 +0530

    Signin function:session

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  
    devise_for :users, path: "api/v1/accounts", controllers: {
-    :registrations 				=> 'api/v1/accounts/registrations'
+    :registrations 				=> 'api/v1/accounts/registrations',
+    :sessions					=> 'api/v1/accounts/sessions'
   }
 
 end
